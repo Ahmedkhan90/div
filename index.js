68bb16cec1a9218543b8079739ae088d30b5f7bb
sarmadkamran <kamransarmad9@gmail.com>
2020-12-16 17:39:53 +0500

    Div

---
+++ index.js
@@ -0,0 +1,9 @@
+var number = 0
+
+function plus (){
+    document.getElementById('conting').innerHTML =number++ 
+}
+
+function minus(){
+    document.getElementById('conting').innerHTML =number--
+}
